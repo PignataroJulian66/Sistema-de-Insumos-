4d5f790fff8d86cae19b0b065dcdafcef5b86322
Julian <pignatarojulian66@gmail.com>
2025-10-04 14:49:00 -0300

    Creacion de inicio de sesion y confeccion de BD

---
+++ BLL/GestorUsuarios.cs
@@ -0,0 +1,54 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class GestorUsuarios
+    {
+        Acceso acceso = new Acceso();
+
+        public bool EsEmpleado(int usuario)
+        {
+            SqlParameter[] parametros = new SqlParameter[1];          
+            parametros[0] = new SqlParameter("@ID_Usuario", usuario);
+            bool Esempleado = bool.Parse(acceso.leerEscalar("sp_EsEmpleado", parametros).ToString());
+            return Esempleado;
+        }
+
+        public bool EsProveedor(int usuario)
+        {
+            SqlParameter[] parametros = new SqlParameter[1];
+            parametros[0] = new SqlParameter("@ID_Usuario", usuario);
+            bool esproveedor = bool.Parse(acceso.leerEscalar("sp_EsProveedor", parametros).ToString());
+            return esproveedor;
+        }
+
+        public int InicioSesion(string email, string contraseña)
+        {
+            try
+            {
+                SqlParameter[] parametros = new SqlParameter[2];
+                int Id_usuario = -1;
+                parametros[0] = new SqlParameter("@Email", email);
+                parametros[1] = new SqlParameter("@Contraseña", contraseña);
+                if(acceso.leerEscalar("sp_InicioSesion", parametros) != null)
+                {
+                    Id_usuario = int.Parse(acceso.leerEscalar("sp_InicioSesion", parametros).ToString());          
+                }
+                return Id_usuario;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("error al iniciar sesion" + ex.Message);
+                return -1;
+            }
+           
+        }
+    }
+}
---
+++ DAL/Acceso.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Acceso
+    {
+        SqlConnection conexion = new SqlConnection();
+        SqlCommand comando = new SqlCommand();
+
+        public void conectar()
+        {
+            conexion.ConnectionString = @"Data Source=JULIÁN;Initial Catalog=Insumos-En-Capas;Integrated Security=True";
+            try
+            {
+                conexion.Open();
+                Console.WriteLine("Conexión exitosa");
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Error de conexión" + ex.Message);
+            }
+        }
+
+        public void desconectar()
+        {
+            try
+            {
+                conexion.Close();
+                conexion.Dispose();
+                Console.WriteLine("Desconexión exitosa.");
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Error al desconectar: " + ex.Message);
+            }
+        }
+
+        public SqlTransaction IniciarTransaccion()
+        {
+            conectar();
+            return conexion.BeginTransaction();
+        }
+
+        public void ConfirmarTransaccion(SqlTransaction tx)
+        {
+            try
+            {
+                if (tx != null)
+                {
+                    tx.Commit();
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Error al confirmar la transacción: " + ex.Message);
+            }
+            finally
+            {
+                desconectar();
+            }
+        }
+
+        public void CancelarTransaccion(SqlTransaction tx)
+        {
+            try
+            {
+                if (tx != null)
+                {
+                    tx.Rollback();
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Error al cancelar la transacción: " + ex.Message);
+            }
+            finally
+            {
+                desconectar();
+            }
+        }
+
+        public int escribir(string sp, SqlParameter[] parametro)
+        {
+            SqlTransaction tx = null;
+            int filasAfectadas = 0;
+            try
+            {
+                tx = IniciarTransaccion();
+                comando.Connection = tx.Connection; // Asignar la conexión de la transacción al comando
+                comando.Transaction = tx; // Asignar la transacción al comando
+                comando.CommandType = System.Data.CommandType.StoredProcedure;
+                comando.CommandText = sp;
+                if (parametro != null)
+                {
+                    comando.Parameters.AddRange(parametro);
+                }
+                filasAfectadas = comando.ExecuteNonQuery();
+                ConfirmarTransaccion(tx);
+                return filasAfectadas;
+            }
+            catch (Exception ex)
+            {
+                CancelarTransaccion(tx);
+                Console.WriteLine("error", ex.Message);
+                return 0;
+            }
+        }
+
+        public DataTable leer(string sp, SqlParameter[] parametro)
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter adaptador = new SqlDataAdapter();
+            conectar();
+            comando.Connection = conexion;
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.CommandText = sp;
+            comando.Parameters.Clear();
+
+            if (parametro != null)
+            {
+                comando.Parameters.AddRange(parametro);
+            }
+            adaptador.SelectCommand = comando;
+            adaptador.Fill(dt);
+            desconectar();
+            return dt;
+        }
+
+
+        public object leerEscalar(string sp, SqlParameter[] parametro)
+        {
+            object resultado = null;
+            try
+            {
+                conectar();
+                comando.Connection = conexion;
+                comando.CommandType = System.Data.CommandType.StoredProcedure;
+                comando.CommandText = sp;
+
+                if (parametro != null)
+                {
+                    comando.Parameters.Clear();
+                    comando.Parameters.AddRange(parametro);
+                }
+                resultado = comando.ExecuteScalar();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Error al ejecutar ExecuteScalar: " + ex.Message);
+            }
+            finally
+            {
+                comando.Parameters.Clear();
+                desconectar();
+            }
+            return resultado;
+        }
+    }
+}
