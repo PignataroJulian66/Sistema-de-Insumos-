6b78ed758967aa8a7f78b0fe6f808ebc56aaeabe
Julian <pignatarojulian66@gmail.com>
2025-10-20 21:42:29 -0300

    Creacion de nuevo git, commit de lo hecho hasta ahora

---
+++ Sistema-de-insumos-DAS/FRMInicioSesion.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Sistema_de_insumos_DAS
+{
+    partial class FRMInicioSesion
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FRMInicioSesion));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnIniciarsesion = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnRegistrarse = new System.Windows.Forms.Button();
+            this.lblerror = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Beige;
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 450);
+            this.panel1.TabIndex = 15;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(41, 81);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(174, 141);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.txtContraseña.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtContraseña.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContraseña.ForeColor = System.Drawing.Color.LightGray;
+            this.txtContraseña.Location = new System.Drawing.Point(372, 170);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(371, 20);
+            this.txtContraseña.TabIndex = 14;
+            this.txtContraseña.Text = "Contraseña";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtEmail.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.ForeColor = System.Drawing.Color.LightGray;
+            this.txtEmail.Location = new System.Drawing.Point(372, 96);
+            this.txtEmail.MaxLength = 255;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(371, 20);
+            this.txtEmail.TabIndex = 13;
+            this.txtEmail.Text = "Email";
+            // 
+            // btnIniciarsesion
+            // 
+            this.btnIniciarsesion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.btnIniciarsesion.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIniciarsesion.FlatAppearance.BorderSize = 0;
+            this.btnIniciarsesion.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.btnIniciarsesion.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnIniciarsesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIniciarsesion.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciarsesion.ForeColor = System.Drawing.Color.LightGray;
+            this.btnIniciarsesion.Location = new System.Drawing.Point(300, 258);
+            this.btnIniciarsesion.Name = "btnIniciarsesion";
+            this.btnIniciarsesion.Size = new System.Drawing.Size(408, 40);
+            this.btnIniciarsesion.TabIndex = 12;
+            this.btnIniciarsesion.Text = "Acceder";
+            this.btnIniciarsesion.UseVisualStyleBackColor = false;
+            this.btnIniciarsesion.Click += new System.EventHandler(this.btnIniciarsesion_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.LightGray;
+            this.label4.Location = new System.Drawing.Point(293, 327);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(158, 17);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "¿Desea crear un cuenta?";
+            // 
+            // btnRegistrarse
+            // 
+            this.btnRegistrarse.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.btnRegistrarse.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRegistrarse.FlatAppearance.BorderSize = 0;
+            this.btnRegistrarse.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.btnRegistrarse.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnRegistrarse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegistrarse.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrarse.ForeColor = System.Drawing.Color.LightGray;
+            this.btnRegistrarse.Location = new System.Drawing.Point(457, 327);
+            this.btnRegistrarse.Name = "btnRegistrarse";
+            this.btnRegistrarse.Size = new System.Drawing.Size(161, 27);
+            this.btnRegistrarse.TabIndex = 20;
+            this.btnRegistrarse.Text = "Registrarse";
+            this.btnRegistrarse.UseVisualStyleBackColor = false;
+            // 
+            // lblerror
+            // 
+            this.lblerror.AutoSize = true;
+            this.lblerror.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblerror.ForeColor = System.Drawing.Color.DarkGray;
+            this.lblerror.Image = ((System.Drawing.Image)(resources.GetObject("lblerror.Image")));
+            this.lblerror.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblerror.Location = new System.Drawing.Point(297, 222);
+            this.lblerror.Name = "lblerror";
+            this.lblerror.Size = new System.Drawing.Size(107, 20);
+            this.lblerror.TabIndex = 19;
+            this.lblerror.Text = "Error mensaje";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.LightGray;
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(726, 1);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(36, 31);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.LightGray;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(759, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(42, 31);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.LightGray;
+            this.label3.Location = new System.Drawing.Point(460, -1);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 33);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "LOGIN ";
+            // 
+            // FRMInicioSesion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.btnIniciarsesion);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnRegistrarse);
+            this.Controls.Add(this.lblerror);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FRMInicioSesion";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnIniciarsesion;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnRegistrarse;
+        private System.Windows.Forms.Label lblerror;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Sistema-de-insumos-DAS/FRMInicioSesion.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_de_insumos_DAS
+{
+    public partial class FRMInicioSesion : Form
+    {
+        public FRMInicioSesion()
+        {
+            InitializeComponent();
+            txtContraseña.UseSystemPasswordChar = false;
+            lblerror.Visible = false;
+        }
+       
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
+        private void btnIniciarsesion_Click(object sender, EventArgs e)
+        {
+        }
+
+
+
+        private void msgerror(string v)
+        {
+            lblerror.Text = "       " + v;
+            lblerror.Visible = true;
+        }
+
+       
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txtDNI_Enter(object sender, EventArgs e)
+        {
+            if (txtEmail.Text == "DNI")
+            {
+                txtEmail.Text = string.Empty;
+                txtEmail.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void txtDNI_Leave(object sender, EventArgs e)
+        {
+            if (txtEmail.Text == string.Empty)
+            {
+                txtEmail.Text = "DNI";
+                txtEmail.ForeColor = Color.DimGray;
+            }
+        }
+
+        private void txtContraseña_Enter(object sender, EventArgs e)
+        {
+            if (txtContraseña.Text == "Contraseña")
+            {
+                txtContraseña.Text = string.Empty;
+                txtContraseña.ForeColor = Color.LightGray;
+                txtContraseña.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtContraseña_Leave(object sender, EventArgs e)
+        {
+            if (txtContraseña.Text == "")
+            {
+                txtContraseña.Text = "Contraseña";
+                txtContraseña.ForeColor = Color.LightGray;
+                txtContraseña.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void FRMInicioSesion_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void btnRegistrarse_Click(object sender, EventArgs e)
+        {
+            //FRMRegistrarse aux = new FRMRegistrarse();
+            //aux.Show();
+            //aux.Enabled = true;
+            //this.Hide();
+        }
+
+        private void btnIniciarsesion_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                BLL.GestorUsuarios gu = new BLL.GestorUsuarios();
+                int usuario = gu.InicioSesion(txtEmail.Text, txtContraseña.Text);
+                bool EsEmpleado = gu.EsEmpleado(usuario);
+                bool EsProveedor = gu.EsProveedor(usuario);
+                if (EsProveedor && EsEmpleado)
+                {
+                    this.Controls.Remove(txtEmail);
+                    this.Controls.Remove(txtContraseña);
+                    this.Controls.Remove(btnRegistrarse);
+                    this.Controls.Remove(btnIniciarsesion);
+                    this.Controls.Remove(lblerror);
+                    this.Controls.Remove(label4);
+
+                    Button nuevoBoton = new Button();
+
+                    nuevoBoton.Text = "Ingresar como empleado";
+                    nuevoBoton.Name = "btnEmpleado";
+                    nuevoBoton.Location = new System.Drawing.Point(326, 74);
+                    nuevoBoton.Size = new System.Drawing.Size(417, 68);
+                    nuevoBoton.BackColor = Color.FromArgb(40, 40, 40);
+                    nuevoBoton.ForeColor = Color.White;
+                    nuevoBoton.Click += (s, ev) =>
+                    {
+                        MessageBox.Show("arbir form de empleado:");
+                    };
+                    this.Controls.Add(nuevoBoton);
+
+
+                    Button nuevoBoton2 = new Button();
+                    nuevoBoton2.Text = "Ingresar como proveedor";
+                    nuevoBoton2.Name = "btnProveedor";
+                    nuevoBoton2.Location = new System.Drawing.Point(326, 199);
+                    nuevoBoton2.Size = new System.Drawing.Size(417, 68);
+                    nuevoBoton2.BackColor = Color.FromArgb(40, 40, 40);
+                    nuevoBoton2.ForeColor = Color.White;
+                    nuevoBoton2.Click += (s, ev) =>
+                    {
+                        MessageBox.Show("arbir form de proveedor:");
+                    };
+                    this.Controls.Add(nuevoBoton2);
+                }
+                else if (EsProveedor && !EsEmpleado)
+                {
+                    MessageBox.Show("abrir form de proveedor:");
+                }
+                else if (EsEmpleado && !EsProveedor)
+                {
+                    MessageBox.Show("arbir form de empleado:");
+                }
+                else
+                {
+                    msgerror("Usuario sin rol.");
+                }
+
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Error al iniciar sesión: " + ex.Message);
+            }
+           
+
+        }
+    }
+}
