693dd01e1cf13f7f972b9caeb7740c04e271b08c
Jere <facundojeremias1@gmail.com>
2025-10-25 19:46:48 -0300

    CAPA SEGURIDAD

---
+++ SEGURIDAD/Encriptación.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEGURIDAD
+{
+        public static class Encriptación
+        {
+            public static byte[] Encriptar(string texto)
+            {
+                byte[] datos = Encoding.UTF8.GetBytes(texto);
+                return ProtectedData.Protect(datos, null, DataProtectionScope.CurrentUser);
+            }
+            public static string Desencriptar(byte[] cifrado)
+            {
+                byte[] datos = ProtectedData.Unprotect(cifrado, null, DataProtectionScope.CurrentUser);
+                return Encoding.UTF8.GetString(datos);
+            }
+        }
+}
